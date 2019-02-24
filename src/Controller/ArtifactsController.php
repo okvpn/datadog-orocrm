ffc59ecdf184bbfe3bb0755253c570e9727222e2
Vladimir Tsykun <vtsykun@okvpn.org>
2019-02-24 13:37:35 +0300

    OK-272: Added route to allow download artifact without authorization

--- src/Controller/ArtifactsController.php
+++ src/Controller/ArtifactsController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Okvpn\Bridge\OroDatadogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
@@ -32,5 +34,30 @@ class ArtifactsController extends Controller
         }
 
         return new Response('', 404, ['Content-Type' => 'text/x-log']);
+    }
+
+    /**
+     * @Route(
+     *     "/artifact/{code}/jira",
+     *     name="okvpn_datadog_artifact_jira",
+     *     requirements={"code"="[a-f0-9]{40}"}
+     * )
+     * @param string $code
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function artifactsJiraAction($code, Request $request)
+    {
+        if (!$secret = $this->container->getParameter('datadog.jira_secret')) {
+            return new JsonResponse(['error' => 'Parameter "datadog.secret" can not be empty, please add update your parameter.yml'], 400);
+        }
+
+        $hash = hash_hmac('sha256', $code, $secret);
+        if ($request->headers->get('Authorization') !== $hash) {
+            return new Response('', 401, ['Content-Type' => 'text/x-log']);
+        }
+
+        return $this->artifactsAction($code);
     }
 }
